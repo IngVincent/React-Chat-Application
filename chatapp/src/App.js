5f60d8275e6d6b946ba877b144193c898e5d1801
Vicente <lazvinz21@gmail.com>
2021-11-16 18:45:06 -0600

    giving the final touches

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,27 +1,24 @@
-import {ChatEngine} from 'react-chat-engine';
-import './App.css';
-import { ChatFeed } from './components/ChatFeed.jsx';
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
+import './App.css';
 
-function App() {
+const projectID = '7a77c33d-1d02-4c47-a10b-623fb4ee978e';
 
-if(localStorage.getItem('username')){
- return <LoginForm />
-
-}
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
   return (
-    <div className="App">
-     <ChatEngine
-        height ="100vh"
-        projectID ="7a77c33d-1d02-4c47-a10b-623fb4ee978e"
-        userName="vicente"
-        userSecret="iguanas92"
-        renderCharFeed = {(chatAppProps) =><chatFeed {...chatAppProps}/>} 
-        onNewMessage = {()=> new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3')}
-     />
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
   );
-}
+};
 
 export default App;
